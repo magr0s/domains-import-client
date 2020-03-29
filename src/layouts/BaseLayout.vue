d7a57b56efe0afe45a8e060fce323087eef74b78
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2020-03-29 10:05:36 +0300

    feat(layouts): Add new layout

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'BaseLayout',
+
+  props: {
+    view: {
+      type: String,
+      default: () => ('hHh lpR lFf')
+    }
+  },
+
+  render (h) {
+    return h(
+      'QLayout',
+      {
+        props: {
+          view: this.view
+        }
+      },
+      [
+        this.$slots.header,
+
+        this.$slots.default,
+
+        h(
+          'QPageContainer',
+          [
+            h('RouterView')
+          ]
+        ),
+
+        this.$slots.footer
+      ]
+    )
+  }
+}
+</script>
