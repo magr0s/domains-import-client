7cb9076b689c86c9332f8a2411611c80e98b28f2
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2020-04-01 22:45:32 +0300

    chore(config): Change api URL

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,7 +1,5 @@
 const config = {
-  FIREBASE_API: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5001/domains-import/us-central1/api'
-    : '',
+  FIREBASE_API: 'https://us-central1-domains-import.cloudfunctions.net/api',
 
   API_VERSION: 'v1'
 }
